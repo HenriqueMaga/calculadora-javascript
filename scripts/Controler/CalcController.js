66cbd2ab7067478a3e1b1888745da39ac8cd9527
HenriqueMaga <henrique.ma400@gmail.com>
2019-05-25 08:58:58 -0300

    Classe Controller constroe display ao ser iniciada

--- scripts/Controler/CalcController.js
+++ scripts/Controler/CalcController.js
@@ -1,3 +1,45 @@
 class CalcController{
-
+    constructor(){
+        this._locale = 'pt_BR';
+        this._displayCalcEl = document.querySelector('#display');
+        this._dataEl = document.querySelector('#data');
+        this._horaEl = document.querySelector('#hora');
+        this._currentDate;
+        this.initialize();
+    }
+    initialize(){
+        this.exibirDisplay();
+        setInterval(()=>{
+            this.exibirDisplay();
+        }, 1000)
+                
+    }
+    exibirDisplay() {
+        this.data = this.currentDate.toLocaleDateString(this.locale);
+        this.hora = this.currentDate.toLocaleTimeString(this.locale);
+    }
+    get displayCalc(){
+        this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(valor){
+        this._displayCalcEl.innerHTML = valor;
+    }
+    get hora(){
+        this._horaEl.innerHTML;
+    }
+    set hora(novaHora){
+        this._horaEl.innerHTML = novaHora;
+    }
+    get data(){
+        this._dataEl.innerHTML;
+    }
+    set data(novaData){
+        this._dataEl.innerHTML = novaData;
+    }
+    get currentDate(){
+        return new Date();
+    }
+    set currentDate(novaData){
+        this._currentDate = novaData;
+    }
 }
